67d31fbcc513cd1c6577cada27f3fc1c662ad289
Esther Leytush <eleytush@gmail.com>
2017-01-31 12:24:23 -0500

    add search functionality

---
+++ assets/javascript/search.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  function displaySearchResults(results, store) {
+    var searchResults = $('#search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<li><div><a href="' + item.url + '">' + item.title + '</a></div>';
+        appendString += "<p class='category-list-item-description'>" + item.shortnote + '</p></li>';
+      }
+
+      $(searchResults).html(appendString);
+    } else {
+      $(searchResults).html('<li>No results found</li>');
+    }
+  };
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  };
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    $('#search-box').val(searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('category');
+      this.field('content');
+    });
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+});
--- to_sort.md
+++ to_sort.md
@@ -1,10 +1,6 @@
 Encryption & security
 https://medium.freecodecamp.com/tor-signal-and-beyond-a-law-abiding-citizens-guide-to-privacy-1a593f2104c3#.e0e2bj2on
 
-
-Firebase
-https://medium.freecodecamp.com/firebase-the-great-the-meh-and-the-ugly-a07252fbcf15#.qvowccsxg
-
 https://github.com/getify/You-Dont-Know-JS
 
 http://davidshariff.com/blog/preparing-for-a-front-end-web-development-interview-in-2017/
@@ -16,3 +12,5 @@ https://css-tricks.com/debouncing-throttling-explained-examples/
 https://css-tricks.com/the-difference-between-throttling-and-debouncing/
 
 https://medium.com/@_jh3y/throttling-and-debouncing-in-javascript-b01cad5c8edf#.1j4jog54f
+
+https://medium.freecodecamp.com/how-we-got-a-2-year-old-repo-trending-on-github-in-just-48-hours-12151039d78b#.hbclhcb22
